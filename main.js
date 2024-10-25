5f62db399e278d2897042ccec738314bd3ecef9a
Carlos Eduardo Wessling <107198663+CarlosWessling@users.noreply.github.com>
2024-10-25 10:47:04 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,76 +1,14 @@
+
+import { aleatorio } from "./aleatorio.js";
+import { perguntas } from "./perguntas.js";
+
+
 const caixaPrincipal = document.querySelector(".caixa-principal");
 const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
-
-const perguntas = [
-    {
-        enunciado: "Você se encontra em uma floresta misteriosa e vê dois caminhos diante de você. Qual você escolhe?",
-        alternativas: [
-            {
-                texto: "O caminho iluminado por uma luz suave.",
-                afirmacao: "Você encontrou uma floresta misteriosa e optou pelo caminho claro e seguro.",
-            },
-            {
-                texto: "O caminho envolto em névoa e sombras.",
-                afirmacao: "Você encontrou uma floresta misteriosa e optou seguir pelo caminho misterioso e sombrio",
-            }
-        ]
-    },
-    {
-        enunciado: "Ao longo do caminho, você ouve um som estranho vindo dos arbustos.",
-        alternativas: [
-            {
-                texto: "Investiga o som, preparado para qualquer coisa.",
-                afirmacao: "Ao ouvir um som estranho, enfrenta sem qualquer medo e seguro de sí.",
-            },
-            {
-                texto: "Ignora o som e segue em frente rapidamente.",
-                afirmacao: "Ao ouvir um som estranho, o ignora e segue seu caminho rapidamente.",
-            }
-        ]
-    },
-    {
-        enunciado: "Você encontra um viajante no caminho, ele parece cansado e perdido. O que você faz?",
-        alternativas: [
-            {
-                texto: "Oferece ajuda e compartilha seus mantimentos.",
-                afirmacao: "Encontra um viajante, então oferece ajuda e compartilha seus mantimentos.",
-            },
-            {
-                texto: "Continua sua jornada sem parar, focado em seu destino.",
-                afirmacao: "Encontra um viajante, porém, decide seguir sem parar, focado em seu destino.",
-            }
-        ]
-    },
-    {
-        enunciado: "A noite cai e você precisa encontrar um lugar para descansar. Onde você decide parar?",
-        alternativas: [
-            {
-                texto: "Em uma caverna próxima, que parece segura.",
-                afirmacao: "Em uma tomada de decisão, em encontrar um lugar para passar a noite, você vai acampar em uma caverna que parece segura.",
-            },
-            {
-                texto: "Em uma clareira ao ar livre, sob as estrelas.",
-                afirmacao: "Em uma tomada de decisão, em encontrar um lugar para passar a noite, decide ir para uma clareira ao ar livre, com vista para as estrelas.",
-            }
-        ]
-    },
-    {
-        enunciado: "No final de sua jornada, você encontra um portal mágico. Qual será sua escolha?",
-        alternativas: [
-            {
-                texto: "Entra no portal, curioso sobre o que há do outro lado.",
-                afirmacao: "Já no final da jornada, encontra um portal, que decide entrar pela sua curiosidade.",
-            },
-            {
-                texto: "Evita o portal, preferindo explorar o mundo ao redor.",
-                afirmacao: "Já no final da jornada, encontra um portal, mas decide evitar qualquer risco, pois quer explorar o mundo.",
-            }
-        ]
-    },
-];
+const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
 let atual = 0;
 let perguntaAtual;
@@ -97,7 +35,7 @@ function mostraAlternativas(){
 }
 
 function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = opcaoSelecionada.afirmacao;
+    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
     historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
@@ -107,6 +45,16 @@ function mostraResultado(){
     caixaPerguntas.textContent = "Sua jornada foi!";
     textoResultado.textContent = historiaFinal;
     caixaAlternativas.textContent = "";
+    caixaResultado.classList.add("mostrar");
+    botaoJogarNovamente.addEventListener("click", JogarNovamente);
 }
+
+function JogarNovamente(){
+    atual = 0;
+    historiaFinal = "";
+    caixaResultado.classList.remove("mostrar");
+    mostraPergunta();
+}
+
 
    mostraPergunta();
