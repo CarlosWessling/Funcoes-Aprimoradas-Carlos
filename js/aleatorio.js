41ebb0d729e970d3edda0f0fb9591c2d6618866b
Carlos Eduardo Wessling <107198663+CarlosWessling@users.noreply.github.com>
2024-11-08 10:46:31 -0300

    08/11

--- js/aleatorio.js
+++ js/aleatorio.js
@@ -1,4 +1,10 @@
+const nomes = ["Carlos", "Gabriel", "Miguel", "Sorrisinho"]
+
+
+
 export function aleatorio(lista){
     const posicao = Math.floor(Math.random()*lista.length);
     return lista[posicao];
 }
+
+export const nome = aleatorio(nomes);
--- js/perguntas.js
+++ js/perguntas.js
@@ -6,14 +6,14 @@ export const perguntas = [
                 texto: "O caminho iluminado por uma luz suave.",
                 afirmacao: [
                     "Você encontrou uma floresta misteriosa e optou pelo caminho claro e seguro.",
-                    "Afirmação 2"
+                    "Ao encontrar uma floresta, você preferiu seguir um caminho sem perigos e com toda segurança."
                 ]    
             },
             {
                 texto: "O caminho envolto em névoa e sombras.",
                 afirmacao: [
                     "Você encontrou uma floresta misteriosa e optou seguir pelo caminho misterioso e sombrio",
-                    "Afirmação 2"
+                    "Ao encontrar uma floresta, você preferiu um caminho muito perigoso sem saber o que poderia encontrar nele."
                 ]
             }
         ]
@@ -25,14 +25,14 @@ export const perguntas = [
                 texto: "Investiga o som, preparado para qualquer coisa.",
                 afirmacao: [
                     "Ao ouvir um som estranho, enfrenta sem qualquer medo e seguro de sí.",
-                    "Afirmação 2"
+                    "o escutar um barulho incomum, você o encara com coragem e confiança."
                 ]
             },
             {
                 texto: "Ignora o som e segue em frente rapidamente.",
                 afirmacao: [
                     "Ao ouvir um som estranho, o ignora e segue seu caminho rapidamente.",
-                    "Afirmação 2"
+                    "Após escutar um barulho incomum, você ignora e segue seu percurso."
                 ]
             }
         ]
@@ -44,14 +44,14 @@ export const perguntas = [
                 texto:"Oferece ajuda e compartilha seus mantimentos.",
                 afirmacao: [
                     "Encontra um viajante, então oferece ajuda e compartilha seus mantimentos.",
-                    "Afirmação 2"
+                    "Você encontra um viajante e, então, oferece auxílio, compartilhando parte de seus suprimentos."
                 ]
             },
             {
                 texto: "Continua sua jornada sem parar, focado em seu destino.",
                 afirmacao: [
                     "Encontra um viajante, porém, decide seguir sem parar, focado em seu destino.",
-                    "Afirmação 2"
+                    "Você encontra um viajante, mas decidiu continuar sem paralizações, com o foco no seu destino."
                 ]
             }
         ]
@@ -63,14 +63,14 @@ export const perguntas = [
                 texto: "Em uma caverna próxima, que parece segura.",
                 afirmacao: [
                     "Em uma tomada de decisão, em encontrar um lugar para passar a noite, você vai acampar em uma caverna que parece segura.",
-                    "Afirmação 2"
+                    "Diante da decisão de encontrar um local para passar a noite, você optou por acampar em uma caverna que aparenta ser segura."
                 ]
             },
             {
                 texto: "Em uma clareira ao ar livre, sob as estrelas.",
                 afirmacao: [
                     "Em uma tomada de decisão, em encontrar um lugar para passar a noite, decide ir para uma clareira ao ar livre, com vista para as estrelas.",
-                    "Afirmação 2"
+                    "Diante da decisão de escolher um local para passar a noite, você opta por uma clareira ao ar livre, onde pode ver as estrelas."
                 ]
             }
         ]
