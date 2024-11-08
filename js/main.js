3a41a6328d12b5c281216df3cf0ab21d355bcc48
Carlos Eduardo Wessling <107198663+CarlosWessling@users.noreply.github.com>
2024-11-08 09:30:30 -0300

    08/11

--- js/main.js
+++ js/main.js
@@ -9,48 +9,52 @@ const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
 const botaoJogarNovamente = document.querySelector(".novamente-btn");
 
+
 let atual = 0;
 let perguntaAtual;
 let historiaFinal = "";
 
-function mostraPergunta () {
-    if (atual>= perguntas.length){
-        mostraResultado();
-        return;
-    }
-    perguntaAtual = perguntas[atual];
-    caixaPerguntas.textContent = perguntaAtual.enunciado;
-    caixaAlternativas.textContent = "";
-    mostraAlternativas();
+function mostraPergunta() {
+if (atual >= perguntas.length) {
+mostraResultado();
+return;
+}
+perguntaAtual = perguntas[atual];
+caixaPerguntas.textContent = perguntaAtual.enunciado;
+caixaAlternativas.textContent = "";
+mostraAlternativas();
 }
 
 function mostraAlternativas(){
-    for (const alternativa of perguntaAtual.alternativas){
-        const botaoAlternativas = document.createElement ("button");
-        botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
-        caixaAlternativas.appendChild (botaoAlternativas);
-    }
+for(const alternativa of perguntaAtual.alternativas) {
+const botaoAlternativas = document.createElement("button");
+botaoAlternativas.textContent = alternativa.texto;
+botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
+caixaAlternativas.appendChild(botaoAlternativas);
+}
 }
 
-function respostaSelecionada(opcaoSelecionada){
-    const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
-    historiaFinal += afirmacoes + " ";
-    atual++;
-    mostraPergunta();
+function respostaSelecionada(opcaoSelecionada) {
+const afirmacoes = aleatorio(opcaoSelecionada.afirmacao);
+historiaFinal += afirmacoes + " ";
+atual++;
+mostraPergunta();
 }
 
-function mostraResultado(){
-    caixaPerguntas.textContent = "Sua jornada foi!";
-    textoResultado.textContent = historiaFinal;
-    caixaAlternativas.textContent = "";
-    botaoJogarNovamente.addEventListener("click", JogarNovamente);
+function mostraResultado() {
+caixaPerguntas.textContent = "Em 2049...";
+textoResultado.textContent = historiaFinal;
+caixaAlternativas.textContent = "";
+caixaResultado.classList.add("mostrar");
+botaoJogarNovamente.addEventListener("click", jogarNovamente);
 }
 
-function JogarNovamente(){
-    atual = 0;
-    historiaFinal = "";
-    mostraPergunta();
+function jogarNovamente(){
+atual = 0;
+historiaFinal = "";
+caixaResultado.classList.remove("mostrar");
+mostraPergunta();
 }
 
-   mostraPergunta();
+
+mostraPergunta();
